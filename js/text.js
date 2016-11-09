f45fc3d4c632469ad96be6cd2361e86f5ee0031e
teppichKay <iamcowa@gmail.com>
2016-11-09 15:10:55 +0200

    textarea half completed

--- js/text.js
+++ js/text.js
@@ -1,7 +1,10 @@
+
+/*
 $(document).ready(function() {
     var count = 0;
-    $("#btn").click(function() {
-        var domElement = $('<div><textarea class="card"></textarea></div>')
+    $(".btn-floating").click(function() {
+        var table = document.getElementById(\)
+        var domElement = document.createElement()
         count += 1;
         //var domElement1 = $('<div class="btn-discard"><text>Discard</text></div>')
         //$(this).after(domElement1).after(domElement);
@@ -9,3 +12,22 @@ $(document).ready(function() {
         $(this).after(domElement);
     });
 });
+*/
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+// add cards
+$('.btn-floating').click(function () {
+
+  $('.row')
+  .append('<div class="col s12 m4"><div class="card-panel teal"><textarea id="textarea" class="materialize-textarea"></textarea><div class="card-action"><a class="waves-effect waves-light btn" id="color-change">alert</a><a class="waves-effect waves-light btn" id="archive">archive</a><a class="waves-effect waves-light btn" id="delete">delete</a></div></div></div>')
+
+})
+
+/*
+// click color change(alert) button
+$('#color-change').click(function () {
+  $('.card-panel').css('color', 'yellow');
+})
+*/
